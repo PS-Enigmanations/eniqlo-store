8731e6b5af4829043abdf750ecac6aae5bf093de
ryanvincere <ryanadhitama2@gmail.com>
2024-05-06 22:45:38 +0800

    Migrations transaction

---
+++ db/migrations/20240506132228_create_transaction_table.up.sql
@@ -0,0 +1,12 @@
+-- Create table transactions
+CREATE TABLE "public"."transactions" (
+    "id" varchar(50) NOT NULL,
+    "customer_id" varchar(50) NOT NULL,
+    "total" numeric NOT NULL,
+    "paid" numeric NOT NULL,
+    "change" numeric NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT now(),
+    "updated_at" timestamptz NULL,
+    CONSTRAINT "fk_transactions_customer" FOREIGN KEY ("customer_id") REFERENCES "public"."customers"("id"),
+    CONSTRAINT transactions_pkey PRIMARY KEY (id)
+);
