f9b62287ba3d7ffc6c79b35941488a855d570c7c
ryanvincere <ryanadhitama2@gmail.com>
2024-05-06 21:20:41 +0800

    Migrations for customer

--- db/migrations/20240506131609_create_customer_table.up.sql
+++ db/migrations/20240506131609_create_customer_table.up.sql
@@ -1,12 +1,11 @@
 -- Create table users
-CREATE TABLE "public"."users" (
+CREATE TABLE "public"."customers" (
     "id" varchar(50) NOT NULL,
     "name" varchar(50) NOT NULL,
     "phone_number" varchar(16) NOT NULL,
-    "password" varchar(150) NOT NULL,
     "created_at" timestamptz NOT NULL DEFAULT now(),
     "updated_at" timestamptz NULL,
 	"deleted_at" timestamptz NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
+    CONSTRAINT customers_pkey PRIMARY KEY (id)
 );
-CREATE UNIQUE INDEX idx_credentials_phone_number ON "public"."users" USING btree (phone_number);
+CREATE UNIQUE INDEX idx_customers_phone_number ON "public"."customers" USING btree (phone_number);
