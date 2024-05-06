b5f7ed89f223a0e4feaab01b401be6cd5ea02119
Ryan Adhitama Putra <ryanadhitama2@gmail.com>
2024-05-06 21:14:42 +0800

    Migration for table products (#2)

---
+++ db/migrations/20240506125211_create_product_table.up.sql
@@ -0,0 +1,17 @@
+-- Create table products
+CREATE TABLE "public"."products" (
+    "id" varchar(50) NOT NULL,
+    "name" varchar(30) NOT NULL,
+    "sku" varchar(30) NOT NULL,
+    "category" varchar(20) NOT NULL,
+    "image_url" varchar[] NOT NULL,
+    "notes" varchar(200) NOT NULL,
+    "price" numeric NOT NULL,
+    "stock" integer NOT NULL,
+    "location" varchar(200) NOT NULL,
+    "is_available" boolean NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT now(),
+    "updated_at" timestamptz NULL,
+	"deleted_at" timestamptz NULL,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+);
