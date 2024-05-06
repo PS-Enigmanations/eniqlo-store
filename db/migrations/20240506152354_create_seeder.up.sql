7d8359061c4d4c3d858114887900c4ab5a4f6f3c
ryanvincere <ryanadhitama2@gmail.com>
2024-05-06 23:34:51 +0800

    Seeder for user

---
+++ db/migrations/20240506152354_create_seeder.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO "public"."users" ("id", "name", "phone_number", "password", "created_at", "updated_at", "deleted_at") 
+VALUES 
+('7a5da8e5-8f41-4e22-bdb9-d3c63f0b2f6e', 'John Doe', '1234567890', '$2a$08$byUr0FmFYtz8zVp7RzsU8.ASjdwAKGAwL6n.nPU6J4g6VNpDx/utu', NOW(), NULL, NULL),
+('b19ab0d0-0c49-47ff-8575-4a34a72b0e17', 'Jane Smith', '9876543210', '$2a$08$byUr0FmFYtz8zVp7RzsU8.ASjdwAKGAwL6n.nPU6J4g6VNpDx/utu', NOW(), NULL, NULL),
+('f2bb0d18-8ef3-4d7a-a2fc-0744f13e32b7', 'Alice Johnson', '5551234567', '$2a$08$byUr0FmFYtz8zVp7RzsU8.ASjdwAKGAwL6n.nPU6J4g6VNpDx/utu', NOW(), NULL, NULL);
