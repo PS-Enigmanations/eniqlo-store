c4d24bf8266fdae06913a39dfbe2be6a11086ab7
Alfin S <benjaminstwo@gmail.com>
2024-05-09 12:47:34 +0800

    feat: add support product full text search

--- db/migrations/20240506125211_create_product_table.down.sql
+++ db/migrations/20240506125211_create_product_table.down.sql
@@ -1,1 +1,5 @@
 DROP TABLE IF EXISTS "public"."products";
+
+DROP TRIGGER products_vector_update ON "public"."products";
+DROP INDEX products_search;
+ALTER TABLE "public"."products" DROP COLUMN "_search";
---
+++ internal/product/product.go
@@ -0,0 +1,32 @@
+package product
+
+import (
+	"time"
+)
+
+type Product struct {
+	Id          string   `json:"id"`
+	Name        string   `json:"name" validate:"required,min=1,max=30"`
+	Sku         string   `json:"sku" validate:"required,min=1,max=30"`
+	Category    Category `json:"category" validate:"required,oneof=Clothing Accessories Footwear Beverages"`
+	ImageUrl    string   `json:"imageUrl" validate:"required,dive,url"`
+	Notes       string   `json:"notes" validate:"required,min=1,max=200"`
+	Price       int      `json:"price" validate:"required,min=1"`
+	Stock       int      `json:"stock" validate:"required,min=0,max=100000"`
+	Location    string   `json:"location" validate:"required,min=1,max=200"`
+	IsAvailable bool     `json:"isAvailable"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
+
+type Category string
+
+// @TODO: Need separate table to store categories `product_types`, `product_type_products`
+const (
+	Clothing    Category = "Clothing"
+	Accessories Category = "Accessories"
+	Footwear    Category = "Footwear"
+	Beverages   Category = "Beverages"
+)
