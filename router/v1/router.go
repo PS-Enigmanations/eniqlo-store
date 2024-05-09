c171a97c00d508e54200ccf3d81b2e450561f6b5
Alfin S <benjaminstwo@gmail.com>
2024-05-09 15:53:03 +0800

    fix git conflicts

--- router/v1/router.go
+++ router/v1/router.go
@@ -13,18 +13,27 @@ type V1Router interface {
 }
 
 type v1Router struct {
-	Product *ProductRouter
+	Product  *ProductRouter
+	Customer *CustomerRouter
 }
 
 func NewV1Router(ctx context.Context, pool *pgxpool.Pool) *v1Router {
 	return &v1Router{
-		Product: NewProductRouter(ctx, pool),
+		Product:  NewProductRouter(ctx, pool),
+		Customer: NewCustomerRouter(ctx, pool),
 	}
 }
 
 func (v *v1Router) Load(router *gin.Engine, m middleware.Middleware) {
 	v1 := router.Group("/v1")
 	{
+		// Customer api endpoint
+		customer := v1.Group("/customer")
+		{
+			customer.POST("/register", v.Customer.Controller.CustomerRegister)
+		}
+
+		// Product api endpoint
 		product := v1.Group("/product")
 		{
 			product.GET("/customer", v.Product.Controller.SearchProducts)
