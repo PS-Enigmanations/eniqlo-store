5f48054ebecdf068e9ca730ffa23155cca9ac0b2
Alfin S <benjaminstwo@gmail.com>
2024-05-11 14:17:31 +0800

    remove unecessary sql query

--- db/migrations/20240506125211_create_product_table.down.sql
+++ db/migrations/20240506125211_create_product_table.down.sql
@@ -2,4 +2,3 @@ DROP TABLE IF EXISTS "public"."products";
 
 DROP TRIGGER products_vector_update ON "public"."products";
 DROP INDEX products_search;
-ALTER TABLE "public"."products" DROP COLUMN "_search";
